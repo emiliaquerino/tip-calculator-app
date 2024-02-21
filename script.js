19b1408bd5f78a3981883635d9c99335d3233d82
Emília Querino <emiliaquerino@gmail.com>
2024-02-21 02:02:58 -0300

    Add um ouvinte de eventos click a cada botão de tip-section

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-let valueBill = 0;
+let billValue = 0;
 
 document.getElementById('input1').addEventListener('input', function(event) {
-  valueBill = parseFloat(this.value);
+  billValue = parseFloat(this.value);
 });
 
-  
+document.getElementById('button5').addEventListener('click', function(event) {
+  calculateSelectedTip(0.05);
+});
+
+document.getElementById('button10').addEventListener('click', function(event) {
+  calculateSelectedTip(0.10);
+});
+
+document.getElementById('button15').addEventListener('click', function(event) {
+  calculateSelectedTip(0.15);
+});
+
+document.getElementById('button25').addEventListener('click', function(event) {
+  calculateSelectedTip(0.25);
+});
+
+document.getElementById('button50').addEventListener('click', function(event) {
+  calculateSelectedTip(0.50);
+});
+
+document.querySelector('.tip-input').addEventListener('input', function(event) {
+  calculateSelectedTip(parseFloat(this.value) / 100);
+});
+
+function calculateSelectedTip(percentage) {
+  let percentageValue = billValue * percentage;
+  console.log(percentageValue);
+}

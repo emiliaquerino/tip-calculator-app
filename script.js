744094d7017a1808046627061ce42fed948e9241
Emília Querino <emiliaquerino@gmail.com>
2024-02-21 14:44:21 -0300

    Add getElement para input2

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
 let billValue = 0;
+let numberOfPeople = 0;
+let total = 0;
 
-document.getElementById('input1').addEventListener('input', function(event) {
+document.getElementById("input1").addEventListener("input", function (event) {
   billValue = parseFloat(this.value);
 });
 
-document.getElementById('button5').addEventListener('click', function(event) {
+document.getElementById("button5").addEventListener("click", function (event) {
   calculateSelectedTip(0.05);
 });
 
-document.getElementById('button10').addEventListener('click', function(event) {
-  calculateSelectedTip(0.10);
+document.getElementById("button10").addEventListener("click", function (event) {
+  calculateSelectedTip(0.1);
 });
 
-document.getElementById('button15').addEventListener('click', function(event) {
+document.getElementById("button15").addEventListener("click", function (event) {
   calculateSelectedTip(0.15);
 });
 
-document.getElementById('button25').addEventListener('click', function(event) {
+document.getElementById("button25").addEventListener("click", function (event) {
   calculateSelectedTip(0.25);
 });
 
-document.getElementById('button50').addEventListener('click', function(event) {
-  calculateSelectedTip(0.50);
+document.getElementById("button50").addEventListener("click", function (event) {
+  calculateSelectedTip(0.5);
 });
 
-document.querySelector('.tip-input').addEventListener('input', function(event) {
-  calculateSelectedTip(parseFloat(this.value) / 100);
-});
+document
+  .querySelector(".tip-input")
+  .addEventListener("input", function (event) {
+    calculateSelectedTip(parseFloat(this.value) / 100);
+  });
 
 function calculateSelectedTip(percentage) {
   let percentageValue = billValue * percentage;
-  console.log(percentageValue);
+  let tipAmount = percentageValue / numberOfPeople;
+  document.querySelector('.display-tip-amount').textContent = `$${tipAmount.toFixed(2)}`;
 }
+
+document.getElementById("input2").addEventListener("input", function (event) {
+  numberOfPeople = parseFloat(this.value);
+});
